11a98d105a090f3ca85bfd946e6c747097d31bca
43h <cw.clx@outlook.com>
2024-11-24 16:24:00 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package proxy_server
+package main
+
+import (
+	"flag"
+)
+
+var logToFile = flag.Bool("l", false, "log")
+
+func main() {
+	flag.Parse()
+	if initLog(*logToFile) == false {
+		return
+	}
+	defer closeLog()
+
+	if initConf() == false {
+		return
+	}
+
+	if initServer() == false {
+		return
+	}
+	startServer()
+	defer closeServer()
+}
